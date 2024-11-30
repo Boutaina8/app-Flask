2e9bd3bc5a1128e17b9c806c73f7831106b962d3
Boutaina8 <boutainam272@gmail.com>
2024-11-30 20:57:17 +0100

    Initial commit

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Remplacez par votre propre clé API
+
 api_key = "9c557b50038e797c039e3ad6004eb6e7"
 
-# Fonction pour récupérer les données météo depuis l'API
+
 def get_weather(city):
     try:
-        # URL de l'API avec la clé API et la ville dynamique
+        
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=fr'
         
-        # Requête à l'API
+        # API
         response = requests.get(url)
         
         if response.status_code == 200:
-            data = response.json()  # Convertir la réponse en JSON
+            data = response.json()  
             main = data['main']
             weather = data['weather'][0]
             wind = data['wind']
             
-            # Récupérer les informations nécessaires
+            
             return {
                 'temp': main['temp'],
                 'description': weather['description'],
@@ -42,11 +42,11 @@ def index():
     cities = ["Paris", "New York", "Tokyo", "London", "Berlin", "Fès", "Casablanca"]  # Villes par défaut
     
     if request.method == "POST":
-        city = request.form["city"]  # Récupère la ville saisie par l'utilisateur
+        city = request.form["city"]  
         if city:
-            cities = [city]  # Remplacer la liste des villes par la ville saisie
+            cities = [city]  
 
-    # Récupérer les données météo pour chaque ville
+    
     for city in cities:
         city_weather = get_weather(city)
         if city_weather:
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bouta\Downloads\uemf-2024-nodejs-main (8)\FlaskApp\venv
+set VIRTUAL_ENV=C:\Users\bouta\Desktop\PYTHON Projects\FlaskApp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
